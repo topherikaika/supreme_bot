9e4a46e15e2e2952529441250482df6fc16cf383
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-08 15:31:12 +0900

    modified attribute of var browser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer")
 const rand_url = "https://jp.supreme.com/collections/all"
 
 async function start() {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   await page.goto(rand_url)
   await page.click(".button button--default fontWeight-bold bpS-button--s width-100 bg-red--aa c-white")

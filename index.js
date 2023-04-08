19a9be18974a1f23094c0a52d4fa24be40bda219
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-08 15:30:12 +0900

    pulled url out of start function and assigned to var rand_url

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const puppeteer = require("puppeteer")
+const rand_url = "https://jp.supreme.com/collections/all"
 
 async function start() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto("https://jp.supreme.com/collections/all")
+  await page.goto(rand_url)
+  await page.click(".button button--default fontWeight-bold bpS-button--s width-100 bg-red--aa c-white")
   await browser.close()
 }
 

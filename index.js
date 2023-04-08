d10c9e62aaf66bc659c893a709a034a5ea670f4a
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-08 15:10:27 +0900

    modify url path

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const puppeteer = require("puppeteer")
 async function start() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  await page.goto("https://jp.supreme.com/collections/all")
+  await browser.close()
 }
 
 start()
